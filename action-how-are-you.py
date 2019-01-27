3151fe2574df7c040ea8703ab4dda2f7da95eb9d
tm13 <huc-6YC-0zZ-ueS>
2019-01-27 14:52:37 +0100

    changing action

--- action-how-are-you.py
+++ action-how-are-you.py
@@ -2,18 +2,18 @@
 # -*- coding: utf-8 -*-
 from hermes_python.hermes import Hermes
 
-INTENT_HOW_ARE_YOU = "bezzam:how_are_you"
+INTENT_HOW_ARE_YOU = "tmatthomasmartinch:how_are_you"
 
 
 def main():
     with Hermes("localhost:1883") as h:
         h.subscribe_intent(INTENT_HOW_ARE_YOU, how_are_you_callback) \
-         .start()
+            .start()
 
 
 def how_are_you_callback(hermes, intent_message):
     session_id = intent_message.session_id
-    response = "I'm doing great."
+    response = "All good, thanks for asking"
     hermes.publish_end_session(session_id, response)
 
 
